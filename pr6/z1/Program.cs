ea27368ed5b72f64b759f4b5028747e6228bd3dc
Наталья Палий <oioioi33777@gmail.com>
2023-03-25 00:53:06 +0300

    pr6

---
+++ pr6/z1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z1
+{
+    class Phone
+    {
+        public Phone(string model, string number)
+        {
+            Model = model;
+            Number = number;
+        }
+        public string Model
+        {
+            get;
+            set;
+        }
+        public string Number
+        {
+            get;
+            set;
+        }
+        public void Call(string number)
+        {
+            Console.WriteLine($"Вызов по номеру {number}");
+            WriteToLog($"Вызов {number}");
+        }
+        protected void WriteToLog(string text)
+        {
+            File.AppendAllText("log.txt", $"{Model}, {DateTime.Now}: {text}\n");
+        }
+    }
+    class Smartphone : Phone
+    {
+        public Smartphone(string model, string number, double cameraResolution) : base(model, number)
+        {
+            CameraResolution = cameraResolution;
+        }
+        public double CameraResolution
+        {
+            get;
+            set;
+        }
+        public void Shoot()
+        {
+            Console.WriteLine("Снимок сделан");
+            WriteToLog($"Снимок сделан");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone myHomePhone = new Phone("samsung", "89511459380");
+            myHomePhone.Call("85672537414");
+            Smartphone myWorkPhone = new Smartphone("iphone", "80998899878", 80.5);
+            myWorkPhone.Call("112");
+            myWorkPhone.Shoot();
+            Console.ReadKey(true);
+        }
+    }
+
+}
+
---
+++ pr6/z3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z3
+{
+    class Human
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get;
+            set;
+        }
+        public char Gender
+        {
+            get;
+            set;
+        }
+        public Human(string name, int age, char gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+    }
+    class Worker : Human
+    {
+        public string Position
+        {
+            get;
+            set;
+        }
+        public Worker(string name, int age, char gender, string position) : base(name, age, gender)
+        {
+
+            Position = position;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human chel1 = new Human("Алексей Квашонкин", 31, 'М');
+            Console.WriteLine(chel1.Name);
+            Worker chel2 = new Worker("Ариана Лолаева", 26, 'Ж', "комик");
+            Console.WriteLine(chel2.Position);
+            Console.ReadKey(true);
+        }
+    }
+}
